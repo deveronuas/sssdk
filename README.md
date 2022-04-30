957a131de20b3ee1e8c2fe503741ecbf781a83e3
Pranay Joshi <joshi.pranay@gmail.com>
2022-04-30 01:41:35 -0400

    Refactor/fix styling issues (#5)
     Some comments a bit of refactor

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Simple Salesforce SDK
 This is a simplified SDK that can be used as a replacement for Salesforce's connected mobile SDK in your SwiftUI application.
+
 ## Contents
 - [Goals](#Goals)
-- [Requirements](#requirements)
+- [Requirements](#Requirements)
 - [Auth Methods](#Auth-Methods)
   - [configure](#configure)
   - [login](#login)
@@ -16,7 +17,7 @@ This is a simplified SDK that can be used as a replacement for Salesforce's conn
   - [Xcode](#xcode)
 
 
-## Goals:
+## Goals
 * Usable by any Salesforce User
     * Internal as well as Community users
 * Allow OAuth2 Login flow
@@ -33,28 +34,30 @@ This is a simplified SDK that can be used as a replacement for Salesforce's conn
 ## Auth Methods
 
 #### configure
-Saves configuration data in memory, of a shared instance
-```swift 
-  let HOST = "your host url"
-  let CLIENTID = "your client's id"
-  let CLIENTSECRET = "your client's secret"
-  let REDIRECTURI = "your redirect uri"
+Saves configuration data in memory
 
- init(){
-    // Required -> To use the SSSDK framework you must configure it.
-    SSSDK.shared.configure(host: HOST, redirectUri: REDIRECTURI, clientId: CLIENTID, clientSecret: CLIENTSECRET)
-  }
-  ```
+!! Required To use the SSSDK framework you must configure its params before calling other methods
+
+```swift
+let HOST = "your host url"
+let CLIENTID = "your client's id"
+let CLIENTSECRET = "your client's secret"
+let REDIRECTURI = "your redirect uri"
+
+SSSDK.shared.configure(host: HOST, redirectUri: REDIRECTURI, clientId: CLIENTID, clientSecret: CLIENTSECRET)
+```
 
 #### login
-Returns login view with the configured host and redirectUri 
+Returns login view with the configured host and redirectUri. Most common use case would be to present this page in sheet.
+
 ```swift
 @State private var launchLoginView = false
+
 var body: some View {
-  VStack{
-    Button{
+  VStack {
+    Button {
       launchLoginView.toggle()
-    }label: {
+    } label: {
       Text("Login")
     }
     .sheet(isPresented: $launchLoginView) {
@@ -66,12 +69,13 @@ var body: some View {
 
 #### handleAuthRedirect
 This method should be called in your app's handler for the auth redirect URI. It will extract the `access_token`, `refresh_token` and save them to keychain. It also uses Salesforce's introspection endpoint to fetch and store the `expiry` of the `access_token` and saves it to keychain.
+
 ```swift
 ContentView()
   .onOpenURL(perform: { url in
     do {
       try SSSDK.shared.handleAuthRedirect(urlReceived: url)
-    }catch{
+    } catch {
       print(error.localizedDescription)
     }
   })
@@ -79,11 +83,12 @@ ContentView()
 
 #### logout
 Erases `access_token`, `refresh_token` and `expiry` data from keychain
+
 ```swift
 var body: some View {
   Button{
     SSSDK.shared.logout()
-  }label: {
+  } label: {
     Text("Logout")
   }.padding(.all)
 }
@@ -92,15 +97,16 @@ var body: some View {
 #### refershAccessToken
 Refreshes the `access_token`, then updates the keychain.
 It also uses Salesforce's introspection endpoint to fetch and store the `expiry` of the `access_token` and saves it to keychain.
+
 ```swift
 var body: some View {
-  Button{
+  Button {
     do {
       try SSSDK.shared.refershAccessToken()
-    }catch{
+    } catch {
       print(error.localizedDescription)
     }
-  }label: {
+  } label: {
     Text("Refresh Token")
   }.padding(.all)
 }
@@ -108,30 +114,34 @@ var body: some View {
 
 ## Data Methods
 
-All methods below will try and refresh the `access_token` if they receive a 401 response from the server. If the refresh fails, we logout.
+Methods below will try and refresh the `access_token` if they receive a 401 response from the server. If the refresh fails, we logout.
 
 #### fetchData
-Fetches raw data and returns JSON, 
+Fetches raw data and returns JSON data
+
 ```swift
-let fetchQuery = "your SOQL query to fetch the data"
+let fetchQuery = "SELECT Id FROM Account"
+
 var body: some View {
   Button{
     do {
       try SSSDK.shared.fetchData(by: fetchQuery) { data in
-        guard let data = data else {return}
+        guard let data = data else { return }
         print(String(decoding: data, as: UTF8.self))
       }
-    }catch{
+    } catch {
       print(error.localizedDescription)
     }
-  }label: {
-    Text("Fetch Job")
+  } label: {
+    Text("Fetch Accounts")
   }.padding(.all)
 }
 ```
 
 ## Installation
+
 ### Swift Package Manager
+
 Add the following line to the `dependencies` in your [`Package.swift`](https://developer.apple.com/documentation/swift_packages/package) file:
 
 ```swift
@@ -160,7 +170,6 @@ let package = Package(
         .target(name: "MyTarget", dependencies: ["sssdk"])
     ]
 )
-
 ```
 
 ### Xcode
--- Sources/SimpleSalesforceSDK/SSSDK.swift
+++ Sources/SimpleSalesforceSDK/SSSDK.swift
@@ -2,12 +2,12 @@ import Foundation
 import SwiftUI
 
 @available(iOS 14.0, *)
-// custom runtime error
+
+/// Custom runtime error. Raised when confoguration is missing or invalid.
 public enum ConfigurationError: Error {
   case runtimeError(String)
 }
 
-///
 /// SSSDK is a class to help to create the OAuth 2.0 user-agent flow,
 /// users authorize a mobile app to access data using an embedded browser.
 /// This OAuth 2.0 flow can replace the Salesforce's connected mobile SDK,
@@ -15,7 +15,8 @@ public enum ConfigurationError: Error {
 ///
 /// Once authenticated, you can also use this class to make API calls to Salesforce.
 public class SSSDK {
-  /// Default SSSDK access
+
+  /// SSSDK is a singleton, use this variable to access it's methods
   public static let shared = SSSDK()
 
   // MARK: Representation Properties
@@ -27,51 +28,63 @@ public class SSSDK {
 
   private init() {}
 
-  /// Saves configuration data in memory, of a shared instance
+  private func confirmConfiguration() throws {
+    if host == nil || host == "" ||
+        clientId == nil || clientId == "" ||
+        clientSecret == nil || clientSecret == "" ||
+        redirectUri == nil || redirectUri == "" {
+      throw ConfigurationError.runtimeError("SSSDK not configured yet")
+    }
+  }
+
+  /// Configure the SDK, these values are stored in memory
   /// - Parameters:
-  ///     - host: The Salesforce instance’s endpoint.
-  ///     - redirectUri: The URL where users are redirected after a successful authentication.
+  ///     - host: The Salesforce instance’s endpoint (or that of the experience cloud community)
+  ///     - redirectUri: The URL where users are redirected after a successful authentication
   ///     - clientId: Client identifier for the OAuth 2.0 client.
   ///     - clientSecret : Client Secret for the OAuth 2.0 client.
-  /// - Note:
-  ///        For redirectUri you have to create URL scheme.
-  ///        This way, any URL with this scheme would start your App.
-  ///        To do so, go to your project file in Xcode.
-  ///        Select your target, go to “Info,” and scroll down until you find “URL Types.”
-  ///         Hit the + button, and add your scheme under “URL schemes.”
+  ///
+  /// The `redirectUri` should match your app's deeplink URL scheme (e.g. `com.company.yourapp://auth`)
+  /// Once the login flow concludes this Uri should launch your app
+  /// and you will receive tokens as query parameters.
+  /// To setup your URL Scheme:
+  ///   1. Go to your project file in Xcode
+  ///   2. Select your target, and go to “Info”
+  ///   3. Scroll down until you find “URL Types”
+  ///   4. Hit the + button, and add your scheme under “URL schemes”
+  /// Also, be sure to add this URL to your redirect uri settings in Salesforce's connected app
   public func configure(host: String, redirectUri: String, clientId: String, clientSecret: String) {
     self.host = host
     self.redirectUri = redirectUri
     self.clientId = clientId
     self.clientSecret = clientSecret
   }
-  /// returns login view with the configured host and redirectUri
-  /// throws
+
+  /// Use this method to show a login flow
+  /// - Returns: A LoginView SwiftUI view object with the configured host and redirectUri
+  /// - Throws: `ConfigurationError.runtimeError` if the singleton is missing the required configuration
   public func loginView() throws -> some View {
-    /// checks weather the SSSDK configured or not
-    guard let host = host, let clientId = clientId, let redirectUri = redirectUri else {
-      throw ConfigurationError.runtimeError("SSSDK not configured yet")
-    }
+    try! confirmConfiguration()
     
-    let url = URL(string: "\(host)/oauth2/authorize?" +
+    let url = URL(string: "\(host!)/oauth2/authorize?" +
                   "response_type=token" +
-                  "&client_id=\(clientId)" +
-                  "&redirect_uri=\(redirectUri)" +
+                  "&client_id=\(clientId!)" +
+                  "&redirect_uri=\(redirectUri!)" +
                   "&mystate=mystate")!
 
     return LoginView(url: url)
   }
-  /// This method should be called in your app's handler for the auth redirect URI.
-  /// It will extract the access_token, refresh_token and save them to keychain.
+
+  /// This method should be called within your app's handler for the auth redirect URI.
+  /// It will extract the `access_token` and `refresh_token`, by parsing the supplied URL.
+  /// Then save them to keychain.
   /// - Parameters:
-  ///     - urlReceived:  Salesforce redirected callback URL.
-  /// - Note:
-  ///     SwiftUI 2.0 comes with a new modifier, onOpenURL.This modifier is available in any View.
-  ///      Whereas you could simply implement onOpenURL on any View that might need it,
-  ///      it’s not a good idea — you’ll most likely be repeating code to handle the deep link,
-  ///      that is, to determine the action to be performed for a particular link.
-  ///      My proposed solution is to use App to do this handling and use the environment
-  ///      to propagate the link and react to that change accordingly.
+  ///     - urlReceived: Full URL received by your handler
+  ///
+  /// SwiftUI 2.0 comes with a new modifier, onOpenURL.This modifier is available to all Views.
+  /// Although, you could implement onOpenURL on any View, we recommend against it.
+  /// Our recommended solution is to use the `App` to handle the URL and use the environment
+  /// observables to propagate and react to that changes accordingly.
   public func handleAuthRedirect(urlReceived: URL) {
     let url = urlReceived.absoluteString
     var urlComponents: URLComponents? = URLComponents(string: url)
@@ -82,7 +95,7 @@ public class SSSDK {
         let temp = queryItems.reduce(into: [String: String]()) { (result, item) in
           result[item.name] = item.value
         }
-        let accessToken = (temp["access_token"] ?? "")  as String
+        let accessToken = (temp["access_token"] ?? "") as String
         let refreshToken = ((temp["refresh_token"] ?? "") as String)
 
         KeychainService.setAccessToken(accessToken)
@@ -90,51 +103,47 @@ public class SSSDK {
       }
     }
   }
-  /// Erases access token, refresh token and expiry date from keychain
+  
+  /// Erases all tokens and expiry date from keychain
   public func logout() {
     KeychainService.clearAll()
   }
-
+  
+  /// This method can be called at anytime to refresh the OAuth access token from the server.
+  /// It will extract the new `access_token` and store it in memory for use with API calls
+  /// - Throws: `ConfigurationError.runtimeError` if the singleton is missing the required configuration
   public func refershAccessToken() throws {
-    /// checks weather the SSSDK configured or not
-    guard let host = host, let clientId = clientId, let clientSecret = clientSecret else {
-      throw ConfigurationError.runtimeError("SSSDK not configured yet")
-    }
+    try! confirmConfiguration()
+    
     guard let refreshToken = KeychainService.refreshToken else { return }
 
-    /// Refreshes the access_token, then update it in the keychain. It also uses Salesforce's introspection endpoint to fetch and store the expiry of the access_token and saves it to keychain.
-    /// - Parameters:
-    ///     - host: The Salesforce instance’s endpoint.
-    ///     - clientId: Client identifier for the OAuth 2.0 client.
-    ///     - clientSecret : Client Secret for the OAuth 2.0 client.
-    ///     - refreshToken : The refresh token issued to the client.
-    /// - Returns: A new access token to the client.
-    WebService.shared.refreshAccessToken(host: host, clientId: clientId, clientSecret: clientSecret, refreshToken: refreshToken)
+    WebService.shared.refreshAccessToken(
+      host: host!,
+      clientId: clientId!,
+      clientSecret: clientSecret!,
+      refreshToken: refreshToken
+    )
   }
 
   public func fetchData(by query: String, completionHandler: @escaping ((Data?) -> Void)) throws {
-    /// checks weather the SSSDK configured or not
-    guard let host = host, let clientId = clientId, let clientSecret = clientSecret else {
-      throw ConfigurationError.runtimeError("SSSDK not configured yet")
-    }
+    try! confirmConfiguration()
     
     guard let accessToken = KeychainService.accessToken else {
       throw ConfigurationError.runtimeError("Access Token missing")
     }
+    
     guard let refreshToken = KeychainService.refreshToken else {
       throw ConfigurationError.runtimeError("Refresh Token missing")
     }
     
-    /// Requests the data using SOQL Query from the salesforce database.
-    /// - Parameters:
-    ///     - host: The Salesforce instance’s endpoint.
-    ///     - clientId: Client identifier for the OAuth 2.0 client.
-    ///     - clientSecret : Client Secret for the OAuth 2.0 client.
-    ///     - refreshToken : The refresh token issued to the client.
-    ///     - accessToken : The access token issued by the authorization server.
-    ///     - query : SOQL query to fetch the data.
-    /// - Returns: Data from the salesforce database.
-    WebService.shared.fetchData(host: host, clientId: clientId, clientSecret: clientSecret, refreshToken: refreshToken, accessToken: accessToken, query: query) { data in
+    WebService.shared.fetchData(
+      host: host!,
+      clientId: clientId!,
+      clientSecret: clientSecret!,
+      refreshToken: refreshToken,
+      accessToken: accessToken,
+      query: query
+    ) { data in
       completionHandler(data)
     }
   }
