2231871f4ca56c7eb705651bee978bbc397999b2
Asheshp23 <asheshp23@gmail.com>
2022-08-22 14:27:34 -0400

    Updated makeRequest method 2

--- Sources/SimpleSalesforceSDK/Services/WebService.swift
+++ Sources/SimpleSalesforceSDK/Services/WebService.swift
@@ -83,16 +83,13 @@ class WebService {
     let statusCode = httpResponse.statusCode
     guard statusCode >= 200 && statusCode < 299 || (ignore401 && statusCode == 401) else {
       let decoder = JSONDecoder()
-      do {
+      if statusCode == 401 {
         let response = try decoder.decode(ResponseError.self, from: data)
         if response.error == "invalid_grant" &&
             response.errorDescription == "expired access/refresh token" {
           SFAuth().reset()
           throw SSSDKError.authRefreshTokenExpiredError
         }
-      } catch {
-
-        throw SSSDKError.duplicateValueError(desc: data)
       }
       throw SSSDKError.notOk(desc: String(decoding: data, as: UTF8.self))
     }
